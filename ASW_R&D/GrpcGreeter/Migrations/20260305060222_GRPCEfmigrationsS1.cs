f3e73f771122f03e0397e2a2372413f573293f39
Shau49-SHLPL <shaurya@shlpl.in>
2026-04-07 11:22:19 +0530

    gRPC Server and gRPC Client - .NET to .NET communication demo

---
+++ ASW_R&D/GrpcGreeter/Migrations/20260305060222_GRPCEfmigrationsS1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace GrpcGreeter.Migrations
+{
+    /// <inheritdoc />
+    public partial class GRPCEfmigrationsS1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TblResults",
+                columns: table => new
+                {
+                    ResultId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    TestId = table.Column<short>(type: "smallint", nullable: false),
+                    SampleId = table.Column<long>(type: "bigint", nullable: false),
+                    Result = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    ResultDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ResultTime = table.Column<TimeSpan>(type: "time(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TblResults", x => x.ResultId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TblResults");
+        }
+    }
+}
---
+++ ASW_R&D/GrpcGreeter/Services/StreamDataService.cs
@@ -0,0 +1,30 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcGreeter.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GrpcGreeter.Services
+{
+    public class StreamDataService : StreamData.StreamDataBase
+    {
+        private Random random;
+
+        public StreamDataService()
+        {
+            random = new Random();
+        }
+        public override async Task ProcessStreamData(StreamDataInput request,IServerStreamWriter<StreamDataOutput> serverStream, ServerCallContext context)
+        {
+
+            for (int i = 1; i <=10; i++) {
+                var Rnum = random.Next(1, 5);
+
+                await serverStream.WriteAsync(new StreamDataOutput {
+                    StreamDataVal = $"Server Streaming message {i} after interval of {Rnum} seconds."
+                });
+                await Task.Delay(Rnum * 1000);
+            }
+        }
+    }
+}
